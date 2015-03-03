44284aae0978563ae4f03b0ee730343e7d0cc8d1
cikmanjeet <manjeet.singh@chefin.kitchen>
2015-03-02 22:24:07 -0500

    search

---
+++ app/models/SearchDetails.java
@@ -0,0 +1,17 @@
+package models;
+
+
+public class SearchDetails {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5904680028570191877L;
+	private  String keyword;
+	public String getKeyword() {
+		return keyword;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+}
