b6c1f66539a15619ffc5784d1e618686519f74f7
cikmanjeet <manjeet.singh@chefin.kitchen>
2015-03-04 21:31:40 -0500

    changes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -34,6 +34,7 @@ public class Application extends Controller {
 	public static final String USER_ROLE = "user";
 	
 	public static Result index() {
+		
 		return ok(Json.toJson("Welcome To chef in Kitchen "));
 	}
 
